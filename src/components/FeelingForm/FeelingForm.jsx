1f68a890be989e1bbd3651bd524e18d93f1fba97
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-26 18:13:04 -0600

    next button working

--- src/components/FeelingForm/FeelingForm.jsx
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './FeelingForm.css';
+
 function FeelingForm() {
+
+  const [feeling, setFeeling] = useState('')
+
   return (
-    <div>
+    <>
       <h1>How are you feeling today?</h1>
-    </div>
+      <div>
+        <p>Feeling?</p>
+        <form onChange={event => setFeeling(event.target.value)}>
+            <input type="number" min="1" max="5"></input>
+            <Link to="/understanding" className="btn-next">
+              <button>NEXT</button>
+            </Link>          
+        </form>
+      </div>
+    </>
   )
 }
 
