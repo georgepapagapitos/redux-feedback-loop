efab77dc1747b2eb538ab80205affbfd4cc8581c
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-26 18:28:46 -0600

    idk

--- src/components/FeelingForm/FeelingForm.jsx
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -2,23 +2,23 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './FeelingForm.css';
 
-function FeelingForm() {
+function FeelingForm({feedbackReducer}) {
 
   const [feeling, setFeeling] = useState('')
 
   return (
-    <>
+    <div className="container">
       <h1>How are you feeling today?</h1>
       <div>
-        <p>Feeling?</p>
         <form onChange={event => setFeeling(event.target.value)}>
-            <input type="number" min="1" max="5"></input>
-            <Link to="/understanding" className="btn-next">
-              <button>NEXT</button>
-            </Link>          
+          <p>Feeling?</p>
+          <input type="number" min="1" max="5"></input>
+          <Link to="/understanding" className="btn-next">
+            <button onClick={feedbackReducer}>NEXT</button>
+          </Link>          
         </form>
       </div>
-    </>
+    </div>
   )
 }
 
